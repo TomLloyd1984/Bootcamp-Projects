9eba26964f4b72b1e5713d455e6ccec1776b312d
TomLloyd1984 <133768928+TomLloyd1984@users.noreply.github.com>
2023-06-29 15:45:27 +0100

    Update hello_world.py

--- Task 1/hello_world.py
+++ Task 1/hello_world.py
@@ -1,1 +1,6 @@
-
+# Found the documentation and video to be clear on print and inputs.
+name=input("Enter your name: ")
+print(name)
+age=input("Enter your age: ")
+print(age)
+print("Hello World!")

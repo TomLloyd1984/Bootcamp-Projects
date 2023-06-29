a54f3738dd0ec9c2ae9d15a2f42daf3fad509374
TomLloyd1984 <133768928+TomLloyd1984@users.noreply.github.com>
2023-06-29 15:48:45 +0100

    Add files via upload

---
+++ Task 1/menu.py
@@ -0,0 +1,11 @@
+print("Order your 3 favourite items of food:")
+# Unsure if there is a way to ask for all 3 items at once and save as separate variables.
+item1=input("1. ")
+item2=input("2. ")
+item3=input("3. ")
+# I initially tried to print the following as one section but I got a syntax error.
+# Wondering if there is a simpler way than printing 4 times?
+print("Order confirmation! You have ordered:")
+print(item1)
+print(item2)
+print(item3)
